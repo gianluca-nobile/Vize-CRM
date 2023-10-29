4dda88e4705335e2f1c8e1fcc0b3b5eae8ca7b6c
gianluca.nobile <gianluca.nobile@gmail.com>
2023-10-29 15:04:41 +0100

    top bar userlist

--- src/app/page/dashboard/user-list/user-list.component.ts
+++ src/app/page/dashboard/user-list/user-list.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
 
   columns: Array<object> | undefined
   userLists: any;
+  topBarBtn: Array<any> = [];
 
   ngOnInit(): void {
     this.generateColumn();
+    this.getUserList();
+    this.generateTobBarBtn();
+  }
+
+  getUserList(){
     this.userLists = [
       {name: 'prova', name1: 'prova', name2: 'prova', usage: 123, sales: '20%', status: true, active: true,},
       {name: 'prova1', name1: 'prova', name2: 'prova', usage: 988, sales: '10%', status: false, active: false,},
@@ -21,7 +27,6 @@ export class UserListComponent {
       {name: 'prova1', name1: 'prova', name2: 'prova', usage: 988, sales: '10%', status: false, active: false,}
     ]
   }
-
   generateColumn(){
     this.columns = [
       {name: 'name', type: ['string']},
@@ -33,6 +38,14 @@ export class UserListComponent {
       {name: 'active', type: ['enableDisable']},
       {name: 'credits', type: ['add', 'remove']}
     ]
+  }
+
+  generateTobBarBtn(){
+    this.topBarBtn.push({title: 'Add user',  icon: 'bi bi-plus-lg', event_: () => this.openAddModal()});
+  }
+
+  openAddModal(){
+    console.log('open modal')
   }
 
   addCreditsUser(index: number){
